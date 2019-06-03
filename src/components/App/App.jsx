0e6990153c824fd721a30abf1929659552c99c8d
Katherine Lewis <kale8432@colorado.edu>
2019-06-03 09:15:20 -0600

    Initial set up

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(){
+	return (
+		<div className="App">
+			<h1>Presidents and Assholes</h1>
+			<CardContainer />
+		</div>
+	);
+}
+
+export default App;
